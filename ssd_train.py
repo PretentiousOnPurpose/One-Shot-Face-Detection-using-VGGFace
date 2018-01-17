e39bdab436ee7d3f5b39426f9ee1023528f89c69
chawat <chawatyash@gmail.com>
2018-01-18 00:16:07 +0530

    Progress

--- ssd_train.py
+++ ssd_train.py
@@ -20,30 +20,11 @@ sess = tf.InteractiveSession()
 X = tf.placeholder(dtype=tf.float32, shape=((375, 500, 3)))
 
 model = SSD()
-
-s0 = Sequential()
-s0.add(model.get_layer(index=13))
-s0.add(Conv2D(filters=5, kernel_size=3, strides=1, padding='same'))
-
-s1 = Sequential()
-s1.add(model.get_layer(index=19))
-s1.add(Conv2D(filters=5, kernel_size=3, strides=1, padding='same'))
-
-s2 = Sequential()
-s2.add(model.get_layer(index=26))
-s2.add(Conv2D(filters=5, kernel_size=3, strides=1, padding='same'))
-
-s3 = Sequential()
-s3.add(model.get_layer(index=33))
-s3.add(Conv2D(filters=5, kernel_size=3, strides=1, padding='same'))
-
-# s4 = Sequential()
-# s4.add(model.get_layer(index=40))
-# s4.add(Conv2D(filters=20, kernel_size=3, strides=1, padding='same'))
-
-# s5 = Sequential()
-# s5.add(model.get_layer(index=42))
-# s5.add(Conv2D(filters=20, kernel_size=3, strides=1, padding='same'))
+model(X)
+p1 = Conv2D(filters=5, kernel_size=3, strides=1, padding='same')(model.get_layer(index=13).output)
+p2 = Conv2D(filters=5, kernel_size=3, strides=1, padding='same')(model.get_layer(index=19).output)
+p3 = Conv2D(filters=5, kernel_size=3, strides=1, padding='same')(model.get_layer(index=26).output)
+p4 = Conv2D(filters=5, kernel_size=3, strides=1, padding='same')(model.get_layer(index=33).output)
 
 def NMS_S1(p1, p2, p3, p4):
     pred1 = tf.reshape(p1, (-1, 46*62, 5))
@@ -51,15 +32,15 @@ def NMS_S1(p1, p2, p3, p4):
     pred3 = tf.reshape(p3, (-1, 23*31, 5))
     pred4 = tf.reshape(p4, (-1, 11*15, 5))
 
-    Z1 = tf.image.non_max_suppression(boxes = pred1[:, :, :, 1:].reshape((46*62, 4)), scores=pred1[:, :, :, 0].reshape((46*62, )), max_output_size=5, iou_threshold=0.55)
-    Z2 = tf.image.non_max_suppression(boxes = pred2[:, :, :, 1:].reshape((46*62, 4)), scores=pred2[:, :, :, 0].reshape((46*62, )), max_output_size=5, iou_threshold=0.55)
-    Z3 = tf.image.non_max_suppression(boxes = pred3[:, :, :, 1:].reshape((23*31, 4)), scores=pred3[:, :, :, 0].reshape((23*31, )), max_output_size=5, iou_threshold=0.55)
-    Z4 = tf.image.non_max_suppression(boxes = pred4[:, :, :, 1:].reshape((11*15, 4)), scores=pred4[:, :, :, 0].reshape((11*15, )), max_output_size=5, iou_threshold=0.55)
+    Z1 = tf.image.non_max_suppression(boxes = tf.reshape(pred1[:, :, 1:], ((46*62, 4))), scores= tf.reshape(pred1[:, :, 0], ((46*62, ))), max_output_size=5, iou_threshold=0.55)
+    Z2 = tf.image.non_max_suppression(boxes = tf.reshape(pred2[:, :, 1:], ((46*62, 4))), scores= tf.reshape(pred2[:, :, 0], ((46*62, ))), max_output_size=5, iou_threshold=0.55)
+    Z3 = tf.image.non_max_suppression(boxes = tf.reshape(pred3[:, :, 1:], ((23*31, 4))), scores= tf.reshape(pred3[:, :, 0], ((23*31, ))), max_output_size=5, iou_threshold=0.55)
+    Z4 = tf.image.non_max_suppression(boxes = tf.reshape(pred4[:, :, 1:], ((11*15, 4))), scores= tf.reshape(pred4[:, :, 0], ((11*15, ))), max_output_size=5, iou_threshold=0.55)
 
-    A1 = np.array([pred1[0][i] for i in Z1])
-    A2 = np.array([pred2[0][i] for i in Z2])
-    A3 = np.array([pred3[0][i] for i in Z3])
-    A4 = np.array([pred4[0][i] for i in Z4])
+    A1 = np.array([pred1[0][i] for i in sess.run(Z1)])
+    A2 = np.array([pred2[0][i] for i in sess.run(Z2)])
+    A3 = np.array([pred3[0][i] for i in sess.run(Z3)])
+    A4 = np.array([pred4[0][i] for i in sess.run(Z4)])
 
     return tf.concat(values=[A1, A2, A3, A4], axis=1)
 
@@ -72,10 +53,6 @@ def NMS_S2(nms):
 
     return tf.reshape(A1, (-1, 5))
 
-p1 = s0.predict(x=X)
-p2 = s1.predict(x=X)
-p3 = s2.predict(x=X)
-p4 = s3.predict(x=X)
 
 nms = NMS_S1(p1, p2, p3, p4)
 Y = NMS_S2(nms)
